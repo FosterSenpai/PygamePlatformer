9684e897976ecd1f9c4be9f600d7a7810b57aeb5
Foster <fosterjameskiatarae@gmail.com>
2025-09-02 01:31:36 +1200

    feat(main.py) Initialize player and implement movement controls

--- src/main.py
+++ src/main.py
@@ -3,15 +3,19 @@
 # Entry point
 
 import pygame
+from classes.player import Player
 
 # Setup
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 running = True
+player = Player(screen)
+delta_time = 0
 
 # Main Loop
 while running:
+    
     # Poll for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,10 +23,22 @@ while running:
             
     screen.fill("white") # Clear frame
     
-    # Render here
+    # Rendering
+    player.draw()
+    
+    # Controls
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player._position.y -= player._max_speed * delta_time
+    if keys[pygame.K_s]:
+        player._position.y += player._max_speed * delta_time
+    if keys[pygame.K_a]:
+        player._position.x -= player._max_speed * delta_time
+    if keys[pygame.K_d]:
+        player._position.x += player._max_speed * delta_time 
     
     pygame.display.flip() # Update Frame
-    clock.tick(60)
+    delta_time = clock.tick(60) / 1000
 
 pygame.quit()
     
