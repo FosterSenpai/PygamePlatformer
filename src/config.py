b5cd7ee482a95b076e25e909e0f99d8d8e763c2b
Foster <fosterjameskiatarae@gmail.com>
2025-09-13 02:55:57 +1200

    feat(config.py) Add configuration settings for game window and layers

---
+++ src/config.py
@@ -0,0 +1,12 @@
+# Foster Rae
+# 13/09/2025
+# Config/Settings for game and global variables.
+
+# Window Settings
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+# Layer settings (Which layer sprite is drawn on)
+PLAYER_LAYER = 2
+GROUND_LAYER = 1
+
