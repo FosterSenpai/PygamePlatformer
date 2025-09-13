e4c2fdbd0f6a4d3956351ab2724aafe3c07bba30
Foster <fosterjameskiatarae@gmail.com>
2025-09-13 19:42:38 +1200

    feat(game_manager) initialize player attribute and add set_player method

--- src/classes/game_manager.py
+++ src/classes/game_manager.py
@@ -1,9 +1,13 @@
 import pygame
+from creatures.player import Player
 
 class GameManager:
     def __init__(self) -> None:
         self.running = True
+        self.player = None
         
     def update(self, delta_time) -> None:
         pass
-        
+    
+    def set_player(self, player):
+        self.player = player
