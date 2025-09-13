ff715c2b6018326b00926b0e2608352b6a32d97e
Foster <fosterjameskiatarae@gmail.com>
2025-09-13 19:43:27 +1200

    feat(play_screen) add player parameter to PlayScreen and implement event handling

--- src/classes/screens/play_screen.py
+++ src/classes/screens/play_screen.py
@@ -2,11 +2,12 @@ import pygame
 from ..levels.level_1 import Level1
 
 class PlayScreen:
-    def __init__(self, screen) -> None:
+    def __init__(self, screen, player) -> None:
         # Screen Setup
         self._screen = screen
         self._bg_colour = (0,0,0)
         self._current_level = Level1()
+        self._player = player
         
     def draw(self) -> None:
         # Prepare Screen
@@ -17,4 +18,7 @@ class PlayScreen:
         pass
     
     def handle_event(self, event):
-        pass
+        # Send events to be handled by player, check for 'Pause' response.
+        player_result = self._player.handle_event(event)
+        if player_result == "Pause":
+            return player_result
